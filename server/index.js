2540bc9255330b7c68c12fd7a383ae413aca86a3
thea a <ta7384@g.rit.edu>
2024-08-21 00:15:05 -0400

    finish initial setup

--- server/index.js
+++ server/index.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const port = 8080;
+
+app.use(cors());
+
+console.log(app);
+
+app.listen(8080, () => {
+    console.log('server listening to port', port);
+});
+
+//routes
+app.get('/', (req, res) => {
+    res.send('hello from our server!');
+});
