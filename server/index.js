b30e4e75855ae9462168a05967bb4736b474d98a
thea a <ta7384@g.rit.edu>
2024-10-10 13:41:05 -0400

    chore: update docs and secret variables

--- server/index.js
+++ server/index.js
@@ -107,8 +107,8 @@ app.post('/user', async (req, res) => {
   };
   const json = {
       grant_type: "client_credentials",
-      client_id: "4f75feb86a3d49adb5a7df64f4d8780d",
-      client_secret: "ee257029ff0a4e999bd831c347bca096",
+      client_id: process.env.REACT_APP_SPOTIFY_ID,
+      client_secret: process.env.REACT_APP_SPOTIFY_SECRET,
   };
   const data = new URLSearchParams(Object.entries(json)).toString();
 
