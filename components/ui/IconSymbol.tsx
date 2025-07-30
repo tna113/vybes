a871bfd81e71312ce4a9d2574a9fa9baa328c8ba
Thea Arias <99890039+tna113@users.noreply.github.com>
2025-07-29 23:08:27 -0500

    VYBES-44: settings page ui (#4)

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -9,8 +9,10 @@ import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
+  'arrow.left': 'arrow-back',
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  'chevron.left': 'chevron-left',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
   'magnifyingglass': 'search',
