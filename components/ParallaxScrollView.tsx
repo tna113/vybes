b8f3c21e172d0f85c0448766fa87db4eed9af94f
Thea Arias <thea.arias@paramount.com>
2025-07-31 14:48:44 -0500

    VYBES-105: create network service

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -9,7 +9,7 @@ import Animated, {
 
 import {ThemedView} from '@/components/ThemedView';
 import {useBottomTabOverflow} from '@/components/ui/TabBarBackground';
-import {useColorScheme} from '@/hooks/useColorScheme';
+import {useColorScheme} from '@/hooks/ui/useColorScheme';
 
 const HEADER_HEIGHT = 250;
 
